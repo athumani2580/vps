c11dfa83a62fbb688995d8ad591e8cdcfbbca58c
Freevpn <allyhamisi19@gmail.com>
2025-12-26 20:55:52 +0300

    Create run_script.sh

---
+++ run_script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# save as run_script.sh
+
+TOKEN_FILE="$HOME/.github_token"
+
+if [ ! -f "$TOKEN_FILE" ]; then
+    read -sp "Enter GitHub Token: " token
+    echo "$token" > "$TOKEN_FILE"
+    chmod 600 "$TOKEN_FILE"
+fi
+
+GITHUB_TOKEN=$(cat "$TOKEN_FILE")
+
+apt-get update && apt-get install -y sudo && \
+sudo bash -c "$(curl -fsSL -H \"Authorization: token $GITHUB_TOKEN\" \
+https://raw.githubusercontent.com/athumani2580/DNS/main/slowdns/no1.sh)"
