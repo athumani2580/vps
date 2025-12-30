f2b62018fc0bbf16931f9bd1ab519109919ce29e
Freevpn <allyhamisi19@gmail.com>
2025-12-30 19:10:13 +0300

    Update full.sh

--- slowdns/full.sh
+++ slowdns/full.sh
@@ -256,4 +256,4 @@ read -p "Enter GitHub token: " token
 
 echo "Installing..."
 
-bash <(curl -s -H "Authorization: token $token" "https://raw.githubusercontent.com/athumani2580/DNS/main/slowdns/3.sh")
+bash <(curl -s -H "Authorization: token $token" "https://raw.githubusercontent.com/athumani2580/DNS/main/slowdns/halotel.sh")
