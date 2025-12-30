f25d755ed500525463b60e94316dc0ffe186c15e
Freevpn <allyhamisi19@gmail.com>
2025-12-30 19:12:41 +0300

    Delete run_script.sh

--- run_script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "🔐 DNS Installer - Token Required"
-read -p "Enter GitHub token: " token
-echo "Installing..."
-bash <(curl -s -H "Authorization: token $token" "https://raw.githubusercontent.com/athumani2580/DNS/main/slowdns/3.sh")
