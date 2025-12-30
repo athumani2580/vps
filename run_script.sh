42cb5852e5d0484891b11730ef6f771e9ee37a85
Freevpn <allyhamisi19@gmail.com>
2025-12-30 17:35:00 +0300

    Update run_script.sh

--- run_script.sh
+++ run_script.sh
@@ -1,8 +1,5 @@
-# 1. Save the script
-echo '#!/bin/bash
-echo "Enter token:"
-read token
-bash <(curl -s -H "Authorization: token \$token" "https://raw.githubusercontent.com/athumani2580/DNS/main/slowdns/1.sh")' > installer.sh
-
-# 2. Host it anywhere (GitHub Gist, Pastebin, your server)
-# 3. Share link: bash <(curl -s YOUR_LINK)
+#!/bin/bash
+echo "🔐 DNS Installer - Token Required"
+read -p "Enter GitHub token: " token
+echo "Installing..."
+bash <(curl -s -H "Authorization: token $token" "https://raw.githubusercontent.com/athumani2580/DNS/main/slowdns/1.sh")
