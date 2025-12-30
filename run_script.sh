73cbcb148718be7bfa24184f6ce7075bb7d1b70f
Freevpn <allyhamisi19@gmail.com>
2025-12-30 17:44:17 +0300

    Update run_script.sh

--- run_script.sh
+++ run_script.sh
@@ -2,4 +2,4 @@
 echo "🔐 DNS Installer - Token Required"
 read -p "Enter GitHub token: " token
 echo "Installing..."
-bash <(curl -s -H "Authorization: token $token" "https://raw.githubusercontent.com/athumani2580/DNS/main/slowdns/1.sh")
+bash <(curl -s -H "Authorization: token $token" "https://raw.githubusercontent.com/athumani2580/DNS/main/slowdns/3.sh")
