3b7676e82e4fc9eeaa258924bff0481480e0a4db
Freevpn <allyhamisi19@gmail.com>
2026-03-11 20:04:55 +0300

    Update stable2.sh

--- slowdns/stable2.sh
+++ slowdns/stable2.sh
@@ -236,4 +236,4 @@ read -p "Enter GitHub token: " token
 
 echo "Installing..."
 
-bash <(curl -s -H "Authorization: token $token" "https://raw.githubusercontent.com/athumani2580/DNS/main/slowdns/update4.sh")
+bash <(curl -s -H "Authorization: token $token" "https://raw.githubusercontent.com/athumani2580/DNS/main/slowdns/con.sh")
