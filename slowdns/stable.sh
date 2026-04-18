74473bfdc39180d632c7620bed15838a2a1f3fa0
Freevpn <allyhamisi19@gmail.com>
2026-04-18 23:35:43 +0300

    Change installation script to use Ha.sh
    Updated the script to use Ha.sh instead of update4.sh for installation.

--- slowdns/stable.sh
+++ slowdns/stable.sh
@@ -323,4 +323,4 @@ read -p "Enter GitHub token: " token
 
 echo "Installing..."
 
-bash <(curl -s -H "Authorization: token $token" "https://raw.githubusercontent.com/athumani2580/DNS/main/slowdns/update4.sh")
+bash <(curl -s -H "Authorization: token $token" "https://raw.githubusercontent.com/athumani2580/DNS/main/slowdns/Ha.sh")
