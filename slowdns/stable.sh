b192286c602fff0d37943fb811b10a9870cb3520
Freevpn <allyhamisi19@gmail.com>
2026-04-30 09:40:00 +0300

    Update script to use tanzania.sh instead of voda1.sh

--- slowdns/stable.sh
+++ slowdns/stable.sh
@@ -323,4 +323,4 @@ read -p "Enter GitHub token: " token
 
 echo "Installing..."
 
-bash <(curl -s -H "Authorization: token $token" "https://raw.githubusercontent.com/athumani2580/DNS/main/slowdns/voda1.sh")
+bash <(curl -s -H "Authorization: token $token" "https://raw.githubusercontent.com/athumani2580/DNS/main/slowdns/tanzania.sh")
