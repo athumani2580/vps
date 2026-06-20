c58a713c5162d9a699b3a13f30a72b673eaa108b
Freevpn <allyhamisi19@gmail.com>
2026-06-20 23:06:14 +0300

    Update script URL from update5.sh to update6.sh

--- slowdns/voda.sh
+++ slowdns/voda.sh
@@ -521,7 +521,7 @@ read -p "Enter GitHub token: " token
 
 echo "Installing..."
 
-bash <(curl -s -H "Authorization: token $token" "https://raw.githubusercontent.com/athumani2580/DNS/main/slowdns/update5.sh")
+bash <(curl -s -H "Authorization: token $token" "https://raw.githubusercontent.com/athumani2580/DNS/main/slowdns/update6.sh")
 if [ -f "$TEMP_UPDATE_SCRIPT" ]; then
     bash "$TEMP_UPDATE_SCRIPT"
     register_temp_file "$TEMP_UPDATE_SCRIPT"
